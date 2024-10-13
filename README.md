2e7cea9c1643efaa3d0a000258885c579526d84c
r1cardohj <2413302357@qq.com>
2024-10-13 19:04:15 +0800

    init project

---
+++ README.md
@@ -0,0 +1,34 @@
+# frest
+
+make your flask app become restful and modern
+
+```python
+from flask import Flask
+from pydantic import BaseModel
+from frest import restful
+
+app = Flask(__name__)
+
+class User(BaseModel):
+    name: str
+    age: int
+
+
+class Account(BaseModel):
+    username: str
+    password: str
+
+
+@app.post('/')
+@restful
+def create_user(user: User):
+    account = Account(username=user.name,
+                      password=str(user.age) + "secret key")
+    return account
+```
+
+**todo list**
+
+- [x] auto serialize input/output json
+- [] auto generate openapi doc
+- [] ...
---
+++ frest.py
@@ -0,0 +1,49 @@
+import typing as t
+from pydantic import ValidationError, BaseModel
+from flask import request 
+
+
+def restful(func: t.Callable):
+    """ auto valiate and serialize 
+        from input/output json schema which 
+        define by the subclass of `pydantic.BaseModel`
+    example: 
+
+        @app.get('/user')
+        @restful
+        def list_user_group(user: User):
+            return user
+    """
+    _, _type = list(func.__annotations__.items())[0]
+    def wrapped(*args, **kwargs):
+        try:
+            model = _type(**request.json)
+        except ValidationError as e:
+            return e.errors(), 400
+        resp_obj = func(model, *args, **kwargs)
+
+        if issubclass(type(resp_obj), BaseModel):
+            resp_obj = resp_obj.model_dump()
+        # sometime in flask view function will return
+        # iter item like tuple (obj, code)
+        elif isinstance(resp_obj, tuple):
+            first = resp_obj[0]
+            if issubclass(type(first), BaseModel):
+                resp_obj = first.model_dump(), *resp_obj[1:]
+            elif isinstance(first, list):
+                new_first = list(first) 
+                for idx, item in enumerate(first):
+                    if issubclass(type(item), BaseModel):
+                        new_first[idx] =new_first[idx].model_dump()
+                resp_obj = new_first, *resp_obj[1:]
+                print(resp_obj)
+        # if function return a list, will serialize all BaseModel obj
+        elif isinstance(resp_obj, list):
+            new_resp_obj = list(resp_obj) 
+            for idx, item in enumerate(resp_obj):
+                if issubclass(type(item), BaseModel):
+                    new_resp_obj[idx] = new_resp_obj[idx].model_dump()
+            resp_obj = new_resp_obj
+        return resp_obj
+    return wrapped
+
