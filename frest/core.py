fea192bad49ccb2708ddd8beec9d41a98574df93
r1cardohj <2413302357@qq.com>
2024-10-16 23:08:24 +0800

    add: swagger class and init_app method

--- frest/core.py
+++ frest/core.py
@@ -1,8 +1,24 @@
-import typing as t
-from pydantic import ValidationError, BaseModel
-from functools import wraps
-from flask import request 
+"""
+Author: r1cardohj
+Email: houjun447@gmail.com
 
+frest is more like a Unix-style Flask plugin support hot swap at any time
+basically it just do two thing:
+    1. Automatically serialize and deserialize json
+    2. Automatically generate openapi documentation
+
+let us make flask be better if you don't like fastapi...(●'◡'●)
+"""
+
+from functools import wraps
+import typing as t
+
+from pydantic import ValidationError, BaseModel
+from flask import Flask, request
+
+
+# use `restful` to collect the type annotations in view-func firstly
+_meta_endpoint_func_types: t.List = []
 
 def restful(func: t.Callable):
     """ auto valiate and serialize 
@@ -12,12 +28,13 @@ def restful(func: t.Callable):
 
         @app.get('/user')
         @restful
-        def list_user_group(user: User):
+        def list_user(user: User):
             return user
     """
     _type = None
     if func.__annotations__:
-        _, _type = list(func.__annotations__.items())[0]
+        types = list(func.__annotations__.items())
+        _, _type = types[0]
         if not issubclass(_type, BaseModel):
             _type = None
 
@@ -46,7 +63,7 @@ def restful(func: t.Callable):
                     if issubclass(type(item), BaseModel):
                         new_first[idx] =new_first[idx].model_dump()
                 resp_obj = new_first, *resp_obj[1:]
-        # if function return a list, will serialize all BaseModel obj
+        # if function return a list, it will serialize all BaseModel obj in list
         elif isinstance(resp_obj, list):
             new_resp_obj = list(resp_obj) 
             for idx, item in enumerate(resp_obj):
@@ -55,3 +72,51 @@ def restful(func: t.Callable):
             resp_obj = new_resp_obj
         return resp_obj
     return wrapped
+
+
+class Swagger:
+    """ auto generate swagger document,
+    use it like normal flask ext.
+
+    example:
+        app = Flask(__name__)
+        swagger = Swagger(app)
+    """
+    
+
+    def __init__(self,
+                 app: t.Optional[Flask]=None,
+                 title: str='Frest API',
+                 description: t.Optional[str]=None,
+                 version: str='0.0.1'):
+        if app is not None:
+            self.init_app(app)
+
+        # openapi metadata
+        self.info = {}
+        self.info['title'] = title
+        self.info['description'] = description
+        self.info['version'] = version
+        self.servers = {}
+        # route map
+        self.paths = {}
+        self.path_spec_data = []
+
+    def init_app(self, app: Flask):
+        self.app = app
+
+        def _spec_collect_decorator(func: t.Callable):
+            """wrap flask.add_url_rule for collect openapi meta data
+            """
+            @wraps(func)
+            def wrapped(*args, **kwargs):
+                url = args[0]
+                methods = kwargs['methods']
+                endpoint = args[1]
+                view_func = args[2]
+                print(url, methods, endpoint, view_func)
+                self.path_spec_data.append((url, methods, endpoint, view_func))
+                return func(*args, **kwargs)
+            return wrapped
+
+        self.app.add_url_rule = _spec_collect_decorator(self.app.add_url_rule)
